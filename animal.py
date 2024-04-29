3d59de8cb16155faa5219e943872a06c18054ac2
xevworks <alqaaf@lifematics.co.jp>
2024-04-29 23:49:13 +0900

    first commit

--- animal.py
+++ animal.py
@@ -39,3 +39,44 @@ spider.describe_animal()
 
 dog.compare_legs(spider)
 bird.sort_animals()
+class Animal:
+    animals_list = []
+
+    def __init__(self, animal_type, legs):
+        self.animal_type = animal_type
+        self.legs = legs
+        if self.animal_type not in self.animals_list:
+            self.animals_list.append(self.animal_type)
+
+    def display_lists(self):
+        print(self.animals_list)
+
+    def describe_animal(self):
+        print(f"The {self.animal_type} has {self.legs} legs.")
+    
+    def sort_animals(self):
+        self.animals_list.sort()
+        print(self.animals_list)
+    
+    def compare_legs(self, other):
+        if self.legs > other.legs:
+            print(f"{self.animal_type} > {other.animal_type}.")
+        elif self.legs < other.legs:
+            print(f"{self.animal_type} < {other.animal_type}.")
+        else:
+            print(f"{self.animal_type} = {other.animal_type}.")
+    
+
+dog = Animal("Dog", 4)
+spider = Animal("Spider", 8)
+bird = Animal("Bird", 2)
+crab = Animal("Crab", 6)
+
+dog.describe_animal()
+# dog.display_lists()
+
+spider.describe_animal()
+# spider.display_lists()
+
+dog.compare_legs(spider)
+bird.sort_animals()
